c7db5cfdfa1b355483318e0f2d83b5683c401faa
naim6246 <uddinnaim895@gmail.com>
2024-01-02 20:18:00 +0600

    added helm deployment for product service

--- param/env.go
+++ param/env.go
@@ -2,6 +2,7 @@ package param
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -11,4 +12,14 @@ func LookupEnvInt64(key string) (int64, error) {
 		return strconv.ParseInt(val, 10, 64)
 	}
 	return 0, errors.New("not found")
+}
+
+func GetPriceFromConfig(path string) (int64, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println("error while getting price", err)
+		return 0, err
+	}
+	fmt.Println("data", string(data))
+	return strconv.ParseInt(string(data), 10, 64)
 }
